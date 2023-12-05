52a6278f76a5b028977dd5d29df7a0b1f04a5f69
Aftab Alam <aftab0103.be21@chitkara.edu.in>
2023-12-05 23:03:33 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Simple Messageboard app hosted on Fly.io
+
+Link : https://mini-messageboard.fly.dev/ 
+
+Tools: NodeJS, Express, Mongoose, Fly.io, Docker, Routes, EJS View Engine, and CSS
---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+  {
+    text: 'Hey there!',
+    user: "Armando",
+    added: new Date()
+  },
+  {
+    text: 'What\'s up?',
+    user: "Keira",
+    added: new Date()
+  },
+]
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+router.post('/new', function(req, res, next) {
+  const userName = req.body.messageUser;
+  const message = req.body.messageText;
+  messages.push({ text: message, user: userName, added: new Date() });
+  res.redirect('/');
+});
+
+module.exports = router;
